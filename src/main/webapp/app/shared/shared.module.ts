a814dcb094236111c817f5ac6086f4c09be7b579
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-06-21 22:31:20 +0200

    init

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { SharedLibsModule } from './shared-libs.module';
 import { FindLanguageFromKeyPipe } from './language/find-language-from-key.pipe';
 import { TranslateDirective } from './language/translate.directive';
@@ -13,8 +12,63 @@ import { SortByDirective } from './sort/sort-by.directive';
 import { SortDirective } from './sort/sort.directive';
 import { ItemCountComponent } from './pagination/item-count.component';
 
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { getDutchPaginatorIntl } from '../dutch-paginator-intl';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
 @NgModule({
-  imports: [SharedLibsModule],
+  imports: [
+    SharedLibsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+  ],
   declarations: [
     FindLanguageFromKeyPipe,
     TranslateDirective,
@@ -28,7 +82,33 @@ import { ItemCountComponent } from './pagination/item-count.component';
     SortDirective,
     ItemCountComponent,
   ],
+
   exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
     SharedLibsModule,
     FindLanguageFromKeyPipe,
     TranslateDirective,
@@ -42,5 +122,6 @@ import { ItemCountComponent } from './pagination/item-count.component';
     SortDirective,
     ItemCountComponent,
   ],
+  providers: [{ provide: MatPaginatorIntl, useValue: getDutchPaginatorIntl() }],
 })
 export class SharedModule {}
